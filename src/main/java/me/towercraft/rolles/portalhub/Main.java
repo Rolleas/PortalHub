b97d35439b14a8b106f8f4ca666e8c0d4b0a9f55
Rolles <peronsos@gmail.com>
2022-05-23 00:55:13 +0300

    fixed and new event

--- src/main/java/me/towercraft/rolles/portalhub/Main.java
+++ src/main/java/me/towercraft/rolles/portalhub/Main.java
@@ -7,7 +7,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
     public static Main instance;
-
     @Override
     public void onEnable() {
         instance = this;
--- src/main/java/me/towercraft/rolles/portalhub/event/PortalEnterEvent.java
+++ src/main/java/me/towercraft/rolles/portalhub/event/PortalEnterEvent.java
@@ -1,21 +1,42 @@
 package me.towercraft.rolles.portalhub.event;
 
+import org.bukkit.Material;
 import me.towercraft.rolles.portalhub.Main;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerPortalEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class PortalEnterEvent implements Listener {
+    HashMap<String, Boolean> players = new HashMap<>();
 
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onEnterPortal(PlayerPortalEvent event) {
-        Player player = event.getPlayer();
-        player.setVelocity(player.getLocation().getDirection().setY(0).normalize().multiply(-2));
+    private boolean checkPlayerInPortal(Player player) {
+        Material material = player.getLocation().getBlock().getType();
+        return material == Material.PORTAL;
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onMovePlayer(PlayerMoveEvent event) {
         Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getInstance(), () -> {
-            player.chat("/gui open servers");
+            if (checkPlayerInPortal(event.getPlayer())) {
+                event.getPlayer().setVelocity(event.getPlayer().getLocation().getDirection().setY(0).normalize().multiply(-5));
+                players.put(event.getPlayer().getName(), true);
+            }
         }, 5);
+        if (!checkPlayerInPortal(event.getPlayer())) {
+            if (players.containsKey(event.getPlayer().getName()) && players.get(event.getPlayer().getName())) {
+                players.remove(event.getPlayer().getName());
+                Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getInstance(), () -> {
+                    event.getPlayer().performCommand("gui open servers");
+                }, 5);
+            }
+        }
     }
 }
